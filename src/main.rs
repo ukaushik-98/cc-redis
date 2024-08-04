f67ef4fb343375f3af4da4f845913aba79b61880
ukaushik <ukaushik@ebay.com>
2024-08-04 15:13:03 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ async fn main() {
                             Err(_) => panic!("failed to parse input"),
                         };
 
-                        let command: Vec<&str> = command.trim().split("\\r\\n").collect();
+                        let command: Vec<&str> = command.trim().split("\r\n").collect();
 
                         let response = parser(command, &mut db_clone);
 
