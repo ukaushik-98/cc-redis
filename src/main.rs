069695c8c2e1440c2f5c2f16f3fbd07f033ce09b
ukaushik <ukaushik@ebay.com>
2024-08-04 09:45:30 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use std::net::TcpListener;
+use bytes::{Buf, BufMut};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _)) => {
                 println!("accepted new connection");
+                tokio::spawn(async move {
+                    let mut buf: Vec<u8> = vec![255];
+                    let read_stream = stream.read(&mut buf).await;
+                    let _ = stream.write(b"+PONG\r\n").await;
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
