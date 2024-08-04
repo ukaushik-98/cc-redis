f76cb4d8b8c9b0866509e4ddc4171d50dd3ebeaf
ukaushik <ukaushik@ebay.com>
2024-08-04 12:22:12 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use bytes::{Buf, BufMut};
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+use tokio::{io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpListener};
 
+enum RedisCommands {
+    Echo(String),
+    Ping(String),
+    Set(String),
+    Get(String)
+}
+
+fn parser(command: Vec<&str>) -> String {
+    match command[2].to_ascii_lowercase().as_str() {
+        "ping" => {
+            "+PONG\r\n".to_string()
+        },
+        "echo" => {
+            format!("${}\r\n{}\r\n", command[4].len(), command[4])
+        },
+        _ => panic!("unrecognized command")
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,14 +31,26 @@ async fn main() {
             Ok((mut stream, _)) => {
                 println!("accepted new connection");
                 tokio::spawn(async move {
-                    let mut buf: Vec<u8> = vec![255];
                     loop {
-                        let read_stream = stream.read_buf(&mut buf).await.unwrap();
+                        let mut buf = vec![];
+                        let mut buf_reader = BufReader::new(&mut stream);
+                        let read_stream = buf_reader.read_buf(&mut buf).await.unwrap();
+                            
                         if read_stream == 0 {
                             println!("socket closed!");
                             break;
                         }
-                        let _ = stream.write(b"+PONG\r\n").await;
+
+                        let command = match std::str::from_utf8(&buf) {
+                            Ok(s) => s,
+                            Err(_) => panic!("failed to parse input"),
+                        };
+
+                        let command: Vec<&str> = command.trim().split("\\r\\n").collect();
+
+                        let response = parser(command);
+
+                        let _ = stream.write(response.as_bytes()).await;
                     }
                 });
             }
