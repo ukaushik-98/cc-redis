38f8928dab1cc7c93bf9c7db636476b61d285a9d
ukaushik <ukaushik@ebay.com>
2024-08-07 19:06:28 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -97,7 +97,8 @@ async fn main() {
             let mut file = File::open("src/db.txt").await.unwrap();
             let mut file_buffer = vec![];
             let _ = file.read_to_end(&mut file_buffer);    
-            let _ = socket.write(&[format!("${}\r\n", file_buffer.len()).as_bytes(), &file_buffer].concat()).await;
+            // let _ = socket.write(&[format!("${}\r\n", file_buffer.len()).as_bytes(), &file_buffer].concat()).await;
+            let _ = socket.write(format!("${}\r\n", file_buffer.len()).as_bytes()).await;
         },
         None => {
             // let host = "localhost:6380";
