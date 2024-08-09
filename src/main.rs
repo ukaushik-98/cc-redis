869e3b1c400007c3fcfe92bacec9f6545b5e83bd
ukaushik <ukaushik@ebay.com>
2024-08-08 22:54:07 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -161,7 +161,7 @@ async fn main() {
 
                     match command_vec[2].to_ascii_lowercase().as_str() {
                         "psync" => {
-                            let mut file = File::open("src/rdb.txt").await.unwrap();
+                            let mut file = File::open("rdb.txt").await.unwrap();
                             println!("IN PSYNC");
                             let mut file_buffer = vec![];
                             println!("FILE READ: {:?}", file_buffer);
