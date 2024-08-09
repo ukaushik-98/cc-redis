b20c3c9f3c09f3d988020ca6b516521ccc53c4ee
ukaushik <ukaushik@ebay.com>
2024-08-08 21:06:51 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -157,6 +157,10 @@ async fn main() {
                     println!("REQUESTS COMMAND: {:?}", requests);
                     let command_vec: Vec<&str> = command.split("\r\n").collect();
 
+                    if command_vec.len() > 3 {
+                        return;
+                    }
+
                     match command_vec[2].to_ascii_lowercase().as_str() {
                         "psync" => {
                             let mut file = File::open("src/rdb.txt").await.unwrap();
