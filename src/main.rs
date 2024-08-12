5a99ef0096e9e8d1794c6e1b24e017a5295bdbd9
ukaushik <ukaushik@ebay.com>
2024-08-12 16:18:08 -0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -183,7 +183,7 @@ async fn main() {
                             continue;
                         }
 
-                        println!("{:?} COMMAND: {:?}", Instant::now(), command_str);
+                        println!("{:?} COMMAND_STR: {:?}", Instant::now(), command_str);
 
                         let command: Vec<&str> = command_str.trim().split("\r\n").collect();
                         println!("{:?} COMMAND: {:?}", Instant::now(), command);
